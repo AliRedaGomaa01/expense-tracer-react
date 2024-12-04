c56991db877b359cac1a04b72690a7232763a5ca
Ali Reda <alyredagomaa@gmail.com>
2024-12-04 16:48:47 +0200

    add playfair font

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import { useGlobalContext } from "./context/GlobalContext";
+// import { useGlobalContext } from "./context/GlobalContext";
 
 function App() {
-  const { state, dispatch } = useGlobalContext();
+  // const { state, dispatch } = useGlobalContext();
 
   return (
     <BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Playfair: ['"Playfair"', ...fontFamily.sans], // Fallback to default sans-serif fonts
+      },
+    },
   },
   plugins: [],
 }
