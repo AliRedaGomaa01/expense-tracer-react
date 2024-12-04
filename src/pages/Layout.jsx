a727a37cebc43469d162518970b456c44b5b512b
Ali Reda <alyredagomaa@gmail.com>
2024-12-04 21:09:53 +0200

    edit layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -7,16 +7,16 @@ const Layout = () => {
   return (
     <>
       <div className="flex flex-col justify-between min-h-screen">
-        <div className="flex flex-row justify-between items-center bg-my-grad  p-10">
+        <div className="flex flex-row justify-between items-center bg-my-grad  p-10 shadow-lg">
           <img src={logo} alt="logo" width="100px" height={"100px"}/>
           <Nav />
         </div>
 
-        <div className="p-5 mx-auto max-w-[80vw] bg-my-grad rounded-xl">
+        <div className="p-5 mx-auto min-w-[50vw] max-w-[90vw] bg-my-grad rounded-xl  shadow-my-rounded flex items-center justify-center">
           <Outlet />
         </div>
 
-        <div className="flex flex-row justify-center text-center items-center bg-my-grad  p-10">
+        <div className="flex flex-row justify-center text-center items-center bg-my-grad  p-10 shadow-my-upper">
           <Footer />
         </div>
       </div>
