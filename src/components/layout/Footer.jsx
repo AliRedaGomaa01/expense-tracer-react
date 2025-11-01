b2787f3b00ea4e7ff08b7276a11adb5c53ea884a
Ali Reda <alyredagomaa@gmail.com>
2025-11-01 15:45:08 +0200

    update domain

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <>
       <footer >
         <p className="font-bold">
-          Designed & Developed & Deployed by <a href="https://aly-h.com">Ali Hussein</a> &copy; 2024 All Rights Reserved
+          Designed & Developed & Deployed by <a href="https://alyhsn.com">Ali Hussein</a> &copy; 2024 All Rights Reserved
         </p>
       </footer>
     </>
